5e2a51b16e1a5e63c054ff9a34eb2d818f7d6f54
Frederic <olivierjp@protonmail.com>
2025-04-10 13:04:09 -0400

    Add warning implementation in Logger class

--- src/main/java/com/appvenir/infrastructure/system/logger/Logger.java
+++ src/main/java/com/appvenir/infrastructure/system/logger/Logger.java
@@ -9,12 +9,20 @@ public class Logger {
     static final String GREEN = "\u001B[32m";
     static final String GREENRESET = "\u001B[0m";
 
+    static final String YELLOW = "\u001B[33m";
+    static final String YELLOWRESET = "\u001B[0m"; 
+
     static final String BOLD = "\u001B[1m";   // ANSI code for bold
     static final String BOLDRESET = "\u001B[0m"; 
 
     public static void info(String message)
     {
         System.out.println(green(bold("[INFO]")) + " " + message);
+    }
+
+    public static void warn(String message)
+    {
+        System.out.println(yellow(bold("[WARN]")) + " " + message);
     }
 
     public static void error(String message)
@@ -30,6 +38,11 @@ public class Logger {
     private static String green(String value)
     {
         return GREEN + value + GREENRESET;
+    }
+
+    private static String yellow(String value)
+    {
+        return YELLOW + value + YELLOWRESET;
     }
 
     private static String red(String value)
