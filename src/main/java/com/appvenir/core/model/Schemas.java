b1a8ad1e7e4abfd2a85c23893010d84b8b51d9f5
Frederic <olivierjp@protonmail.com>
2025-04-10 13:02:58 -0400

    Update the Schemas class

--- src/main/java/com/appvenir/core/model/Schemas.java
+++ src/main/java/com/appvenir/core/model/Schemas.java
@@ -1,25 +1,43 @@
 package com.appvenir.core.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Setter
-@Getter
-@ToString
 public class Schemas {
-    private List<DirSchema> dirSchema = new ArrayList<>();
+    private List<DirSchema> dirSchemas;
+    private HashMap<String, String> classNameFormat;
 
-    @Getter
-    @Setter
-    @ToString
-    @NoArgsConstructor
-    static class DirSchema {
-        private String id;
-        private List<String> paths = new ArrayList<>();
+    public Schemas()
+    {
+        this.dirSchemas = new ArrayList<>();
+        this.classNameFormat = new HashMap<>();
+    }
+
+    public Optional<DirSchema> getDirSchema(String id)
+    {
+        for(DirSchema dirSchema : dirSchemas)
+        {
+            if(dirSchema.getId().equals(id))
+            {
+                return Optional.of(dirSchema);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public List<DirSchema> getDirSchemas() {
+        return dirSchemas;
+    }
+    public void setDirSchema(List<DirSchema> dirSchema) {
+        this.dirSchemas = dirSchema;
+    }
+    public HashMap<String, String> getClassNameFormat() {
+        return classNameFormat;
+    }
+    public void setClassNameFormat(HashMap<String, String> classNameFormat) {
+        this.classNameFormat = classNameFormat;
     }
 }
