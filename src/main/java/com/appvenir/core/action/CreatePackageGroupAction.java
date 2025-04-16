97a9505d49a9bb41d6fb53b8563db1751ac2abae
Frederic <olivierjp@protonmail.com>
2025-04-16 09:31:33 -0400

    Create PackageGroupCommand & CreatePackageGroupAction

---
+++ src/main/java/com/appvenir/core/action/CreatePackageGroupAction.java
@@ -0,0 +1,135 @@
+package com.appvenir.core.action;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.appvenir.core.PackageName;
+import com.appvenir.infrastructure.system.io.IO;
+import com.appvenir.infrastructure.system.logger.Logger;
+import com.appvenir.utils.StringUtils;
+
+public class CreatePackageGroupAction implements FileAction {
+    private final int MAX_FILE_CREATION = 10;
+    private final Map<String, String>  pathMap;
+    private final String targetPath;
+    private final List<String> paths;
+    private boolean hasFiles = false;
+
+    public CreatePackageGroupAction(Map<String, String> pathMap, String[] fileNames, String targetPath)
+    {
+        if(pathMap.size() > MAX_FILE_CREATION)
+        {
+            throw new IllegalStateException("Maximum path creation reached.");
+        }
+        this.pathMap = pathMap;
+        this.targetPath = targetPath;
+        this.hasFiles = fileNames.length > 0;
+        this.paths = generatePath(fileNames);
+    }
+
+    public CreatePackageGroupAction(Map<String, String> pathMap, String[] fileNames, String targetPath, boolean inverse)
+    {
+        if(pathMap.size() > MAX_FILE_CREATION)
+        {
+            throw new IllegalStateException("Maximum path creation reached.");
+        }
+        this.pathMap = pathMap;
+        this.targetPath = targetPath;
+        this.hasFiles = fileNames.length > 0;
+        this.paths = !inverse ? generatePath(fileNames) : generateInversePath(fileNames);
+    }
+
+    private void createPackages() throws IOException
+    {
+        for(String path : paths)
+        {
+            try {
+                if(hasFiles){
+                    Logger.info("Create file: " + path);
+                    IO.createFileIfNotExist(path);
+                }else {
+                    Logger.info("Create path: " + path);
+                    IO.createPathIfNotExist(path);
+                }
+            } 
+            catch (FileAlreadyExistsException e) {
+                Logger.warn("File path already exists: " + path);
+            }
+        }
+    }
+
+    private void deletePaths() throws IOException
+    {
+        for(String path : paths)
+        {
+            Logger.info("Delete path: " + path);
+            IO.deletePath(path);
+        }
+    }
+
+    public List<String> generatePath(String[] fileNames)
+    {
+
+        List<String> filePaths = new ArrayList<>();
+        for (Map.Entry<String, String> entry : pathMap.entrySet()) 
+        {
+            String packageId = entry.getKey();
+            String currentPackage = entry.getValue();
+
+            PackageName packageName = PackageName.create(currentPackage);
+            String parentPath = StringUtils.convertPackagetoPath(packageName);
+
+            if(hasFiles)
+            {
+                for(String fileName : fileNames)
+                {
+                    String javaFileName = StringUtils.createJavaFile(StringUtils.addPackageIdToFileName(fileName, packageId));
+                    filePaths.add(targetPath + parentPath + "/" + javaFileName);
+                }
+            }else {
+                filePaths.add(targetPath + parentPath);
+            }
+        }
+        return filePaths;
+    }
+
+    public List<String> generateInversePath(String[] fileNames)
+    {
+
+        List<String> filePaths = new ArrayList<>();
+
+        for(String fileName : fileNames) {
+
+            for (Map.Entry<String, String> entry : pathMap.entrySet()) {
+
+                String packageId = entry.getKey();
+                String currentPackage = entry.getValue();
+
+                PackageName packageName = PackageName.create(currentPackage);
+                String lastPackageSegment = StringUtils.getLastPackageSegment(packageName);
+                PackageName featurePackage = StringUtils.replaceLastSegment(packageName, fileName.toLowerCase()).add(lastPackageSegment);
+                String parentPath = StringUtils.convertPackagetoPath(featurePackage);
+                String javaFileName = StringUtils.createJavaFile(StringUtils.addPackageIdToFileName(fileName, packageId));
+                filePaths.add(targetPath + parentPath + "/" + javaFileName);
+            }
+
+        }
+
+        return filePaths;
+    }
+
+    @Override
+    public void execute() throws IOException {
+        createPackages();
+    }
+
+    @Override
+    public void undo() throws IOException{
+        deletePaths();
+    }
+    
+}
+
---
+++ src/main/java/com/appvenir/core/commands/PackageGroupCommand.java
@@ -0,0 +1,62 @@
+package com.appvenir.core.commands;
+
+import java.util.NoSuchElementException;
+import com.appvenir.core.ActiveProjectDetails;
+import com.appvenir.core.action.CreatePackageGroupAction;
+import com.appvenir.core.model.PackageGroup;
+import com.appvenir.core.model.WorkspaceTemplate;
+import com.appvenir.infrastructure.system.logger.Logger;
+import com.appvenir.init.CliContext;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(
+    name = "packages"
+)
+public class PackageGroupCommand extends Reversable implements Runnable{
+
+    @Parameters(
+        index = "0",
+        description = "package group id"
+    )
+    private String packageGroupId;
+
+    @Option(
+        names = {"-f", "--file"},
+        description = "File names"
+    )
+    private String fileNames;
+
+    @Option(
+        names = {"--inverse"},
+        description = "Inverse"
+    )
+    private boolean isInverted;
+
+    @Override
+    public void run() {
+        ActiveProjectDetails activeProjectDetails = CliContext.getActiveProjectDetails();
+        WorkspaceTemplate workspaceTemplate = CliContext.getWorkspaceTemplate();
+
+        PackageGroup packageGroup = workspaceTemplate.getPackageGroup(packageGroupId)
+                    .orElseThrow(() -> new NoSuchElementException("Could not find a package group with id: " + packageGroupId));
+          
+        String[] splitFileNames = fileNames == null || fileNames.isBlank() ? new String[0] : fileNames.split(",");
+        CreatePackageGroupAction createPackageGroupAction = new CreatePackageGroupAction(packageGroup.getPackages(), splitFileNames, activeProjectDetails.getAppRootPath(), isInverted);
+        try {
+            if(!rollback)
+            {
+                createPackageGroupAction.execute();
+            } else {
+                createPackageGroupAction.undo();
+            }
+        } catch (Exception e) {
+            Logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    
+}
