fec104f2ecad26e1f66a20a785cd9b91bb3817b6
Frederic <olivierjp@protonmail.com>
2025-04-16 14:45:08 -0400

    Create JavaClassName

---
+++ src/main/java/com/appvenir/core/valueObjects/JavaClassName.java
@@ -0,0 +1,51 @@
+package com.appvenir.core.valueObjects;
+
+import com.appvenir.exceptions.InvalidJavaClassName;
+import com.appvenir.utils.CliUtils;
+import com.appvenir.utils.StringUtils;
+
+public class JavaClassName {
+    private final String value;
+
+    private JavaClassName(String value)
+    {
+        throwsIfNotValidClassName(value);
+        this.value = StringUtils.capitalizeFirstLetter(value);
+    }
+
+    public String getFileName()
+    {
+        return this.value + ".java";
+    }
+
+    public String getFilePath()
+    {
+        return "/" + this.value + ".java";
+    }
+
+    public String value()
+    {
+        return this.value;
+    }
+
+    public JavaClassName suffixed(String name)
+    {
+        return new JavaClassName(this.value + StringUtils.capitalizeFirstLetter(name));
+    }
+
+    public static JavaClassName create(String name)
+    {
+        return new JavaClassName(name);
+    }
+
+    private final void throwsIfNotValidClassName(String value)
+    {
+        if (!CliUtils.isValidJavaClassName(value)) 
+        {
+            throw new InvalidJavaClassName(value);
+        }
+    }
+    
+}
+
+
